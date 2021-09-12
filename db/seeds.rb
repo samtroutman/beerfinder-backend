0babda9ef421c7eb2818f3b65a742f5cd68fbe7e
samtroutman <troutm16@gmail.com>
2021-09-12 19:38:45 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create(name: "Lager", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/american-lager.jpg")
+Category.create(name: "IPA", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/american-india-pale-ale.jpg")
+Category.create(name: "Pilsner", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/german-style-pilsener.jpg")
+Category.create(name: "Amber", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/amber-ale.jpg")
+Category.create(name: "Stout", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/american-stout.jpg")
+Category.create(name: "Porter", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/english-style-brown-porter.jpg")
+Category.create(name: "Sour", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/american-sour.jpg")
+Category.create(name: "Wheat", image_url: "https://www.craftbeer.com/wp-content/uploads/_SF/web/american-wheat.jpg")
+
+
+
