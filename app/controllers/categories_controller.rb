3fc67539a59815374e1c5aab6a350b049d3d10eb
samtroutman <troutm16@gmail.com>
2021-09-19 19:13:57 -0400

    add show method to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,12 @@ class CategoriesController < ApplicationController
         categories = Category.all
         render json: categories
     end
+
+    def show 
+        category = Category.find_by(id: params[:id])
+        if category 
+            render json: categories
+        else
+            render json: {message: "Category not found."}
+    end
 end
